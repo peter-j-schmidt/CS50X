ff2b799e9290536154b4318c66971ddd45d84691
peter-j-schmidt <schmidt.peter.john@gmail.com>
2020-12-05 22:06:04 -0500

    Added code for login function, basic layout template, and sqlite3 database functionality.

--- Final Project/app.py
+++ Final Project/app.py
@@ -1,5 +1,19 @@
 import os
 
-from flask import Flask
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, g
+from flask_session import Session
+from werkzeug.exceptions import default_esceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
+import datetime
+import sqlite3
+
+# Configure the application
 app = Flask(__name__)
+
+db = sqlite3.connect('stocks.db')
+
+
+@app.route("/")
+# This will be where login_required should go.
+def index():
---
+++ Final Project/helpers.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session, g
+from functools import wraps
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+
+    Based on CS50 Finance helper function.
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
